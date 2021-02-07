22eb662f7197f2198b3e9e38ec1f7d8d96c0f377
Josh Mix <josh.mix@blockfi.com>
2021-02-07 14:30:53 -0500

    mastery api and persistence tests added

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,8 @@
 use Mix.Config
-config: :mastery_persistence,
+
+config :mastery_persistence,
   ecto_repos: [MasteryPersistence.Repo]
+
 config :logger, level: :info
+
+import_config "#{Mix.env()}.exs"
